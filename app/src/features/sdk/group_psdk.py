da2b2aceb6399961eeb406fc4847be7a0b3fb133
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-01-21 00:57:45 +0300

    Install psdk 1

--- app/src/features/sdk/group_psdk.py
+++ app/src/features/sdk/group_psdk.py
@@ -1,3 +1,8 @@
+import os
+import pathlib
+import subprocess
+from pathlib import Path
+
 import click
 
 from app.src.features.sdk.impl.download import multi_download
@@ -22,7 +27,7 @@ def available():
 
 
 @group_psdk.command()
-def download():
+def install():
     """Download and run install Aurora Platform SDK."""
 
     versions = get_map_versions(TypeSDK.PSDK)
@@ -40,5 +45,68 @@ def download():
 
     files = multi_download(files_url)
 
-    click.echo('\nFiles:\n{}'
-               .format(get_string_from_list_numbered(files)))
+    archive_chroot = [item for item in files if 'Chroot' in item and 'tar.bz2' in item]
+    archive_tooling = [item for item in files if 'Tooling' in item]
+    archive_target = [item for item in files if 'Target' in item]
+
+    if not archive_chroot:
+        click.echo('Chroot tar.bz2 not found.', err=True)
+        return
+
+    if not archive_tooling:
+        click.echo('Tooling tar.bz2 not found.', err=True)
+        return
+
+    version = os.path.basename(archive_chroot[0]).split('-')[1]
+    psdk_path = str(Path.home() / 'Aurora_Platform_SDK_{}'.format(version))
+    chroot_path = '{}/sdks/aurora_psdk'.format(psdk_path)
+    chroot = '{}/sdk-chroot'.format(chroot_path)
+
+    if os.path.isdir(psdk_path):
+        click.echo('\nError: Folder already exists: {}'.format(psdk_path), err=True)
+        return
+
+    pathlib.Path(psdk_path).mkdir()
+    pathlib.Path(chroot_path).mkdir(parents=True, exist_ok=True)
+
+    pathlib.Path('{}/toolings'.format(psdk_path)).mkdir()
+    pathlib.Path('{}/tarballs'.format(psdk_path)).mkdir()
+    pathlib.Path('{}/targets'.format(psdk_path)).mkdir()
+
+    click.echo('\nInstall chroot.')
+    subprocess.call([
+        'sudo',
+        'tar',
+        '--numeric-owner',
+        '-p',
+        '-xjf',
+        archive_chroot[0],
+        '--checkpoint=.1000',
+        '-C',
+        chroot_path
+    ])
+
+    click.echo('\nInstall tooling.')
+
+    subprocess.call([
+        chroot,
+        'sdk-assistant',
+        'tooling',
+        'create',
+        '-y',
+        'AuroraOS-{}-base'.format(version),
+        archive_tooling[0]
+    ])
+
+    for target in archive_target:
+        arch = target.split('-')[-1].split('.')[0]
+        click.echo('\nInstall target "{}".'.format(arch))
+        subprocess.call([
+            chroot,
+            'sdk-assistant',
+            'target',
+            'create',
+            '-y',
+            'AuroraOS-{}-base-{}'.format(version, arch),
+            archive_tooling[0]
+        ])
