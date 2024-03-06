e276917e71f9e1e3c6a73b99cbf2e7b0cf835cb4
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-03-06 23:47:02 +0300

    Update text

--- aurora_cli/src/features/flutter/impl/utils.py
+++ aurora_cli/src/features/flutter/impl/utils.py
@@ -112,7 +112,7 @@ PLUGINS_OUTPUT_HTML = '''
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Plugin info</title>
+    <title>Plugins info</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
@@ -139,7 +139,7 @@ PLUGINS_OUTPUT_HTML = '''
             ">
                 <div style="margin-bottom: 10px;">
                     <div style="font-size: 28px; font-weight: 600;">
-                        Plugin info
+                        Plugins info
                     </div>
                     <div style="
                         font-weight: 700;
