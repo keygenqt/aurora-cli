ba1effe79a892bb52c77187f1834a74bf2113143
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-05-22 22:40:32 +0300

    Update license

--- aurora_cli/src/api/device.py
+++ aurora_cli/src/api/device.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 from aurora_cli.src.base.models.device_model import DeviceModel
 from aurora_cli.src.base.output import echo_stdout_json, OutResult
 from aurora_cli.src.common.ssh_features import (
--- aurora_cli/src/api/emulator.py
+++ aurora_cli/src/api/emulator.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 from aurora_cli.src.base.models.emulator_model import EmulatorModel
 from aurora_cli.src.base.output import echo_stdout_json
 from aurora_cli.src.common.ssh_features import (
