7286945181b38b406b9bccff6a189a51154d1a0a
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-06-27 21:11:16 +0300

    Update data.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 long_description = """
 ![picture](https://github.com/keygenqt/aurora-cli/blob/api/data/images/banner/banner_1000.png?raw=true)
 
-[![picture](https://github.com/keygenqt/aurora-cli/blob/api/data/images/banner/banner_1000.png?raw=true)](https://aurora-cli.keygenqt.com/)
+[![picture](https://github.com/keygenqt/aurora-cli/blob/api/data/common/btn.png?raw=true)](https://aurora-cli.keygenqt.com/)
 
 ### License
 
