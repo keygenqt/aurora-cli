487c6604925c2a451a7325ac51930296ca4b23bc
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-09-29 12:11:42 +0300

    [feature] Add test for query to API.

--- aurora_cli/src/api/group_api.py
+++ aurora_cli/src/api/group_api.py
@@ -24,6 +24,7 @@ from aurora_cli.src.api.routes.routes_flutter import search_route_flutter
 from aurora_cli.src.api.routes.routes_psdk import search_route_psdk
 from aurora_cli.src.api.routes.routes_sdk import search_route_sdk
 from aurora_cli.src.api.routes.routes_settings import search_route_settings
+from aurora_cli.src.api.routes.routes_tests import search_route_tests
 from aurora_cli.src.api.routes.routes_vscode import search_route_vscode
 from aurora_cli.src.base.configuration.app_config import AppConfig
 from aurora_cli.src.base.texts.app_argument import TextArgument
@@ -307,6 +308,7 @@ def group_api(route: str):
             search_route_sdk,
             search_route_vscode,
             search_route_settings,
+            search_route_tests,
         ]:
             if func(route):
                 echo_verbose(get_arg_bool(route, 'verbose'))
---
+++ aurora_cli/src/api/routes/routes_tests.py
@@ -0,0 +1,44 @@
+"""
+Copyright 2024 Vitaliy Zarubin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from time import sleep
+
+from aurora_cli.src.base.utils.output import echo_stdout, OutResult, OutResultInfo, OutResultError
+from aurora_cli.src.base.utils.route import get_route_root, get_arg_int_optional
+
+
+def search_route_tests(route: str) -> bool:
+    root = get_route_root(route)
+    if root == '/tests/answer':
+        time = get_arg_int_optional(route, 'time')
+        code = get_arg_int_optional(route, 'code')
+        iterate = get_arg_int_optional(route, 'iterate')
+        if not iterate:
+            iterate = 1
+        for x in range(iterate):
+            if time:
+                sleep(time/1000)
+            if code == 100:
+                echo_stdout(OutResultInfo('Test answer', value={}))
+            elif code == 500:
+                echo_stdout(OutResultError('Test answer', value={}))
+            else:
+                echo_stdout(OutResult('Test answer', value={}))
+    elif root == '/tests/answers':
+        echo_stdout(OutResult('Test answer: {}'.format(1), value={}))
+    else:
+        return False
+
+    return True
