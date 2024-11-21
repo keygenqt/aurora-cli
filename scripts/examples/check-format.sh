9c49eee6913cce2373967fd88550fcd4662ad480
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-11-21 18:30:37 +0300

    [feature] Add example bash API validate. [feature] Add example bahs API check-format.

---
+++ scripts/examples/check-format.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##########################
+## Example check-format CI
+##########################
+
+# Path to project
+project=$1
+
+## Get first installed flutter.
+# We need to understand which flutter we will be accessing.
+version=$(aurora-cli api --route='/flutter/installed' | grep -E "\"[0-9].+" | head -n 1 | xargs | sed 's/,//g')
+
+# Check format
+code=$(aurora-cli api --route="/flutter/project/check-format?path=$project&version=$version" | grep "code" | head -n 1 | xargs | sed 's/,//g' | sed 's/code: //g')
+if [ "$code" == "200" ]; then
+  echo 'There is formatting!'
+  exit 0;
+else
+  echo 'No formatting!'
+  exit 1;
+fi
