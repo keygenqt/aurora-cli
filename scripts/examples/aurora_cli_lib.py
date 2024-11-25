cab9c841169f544ade0edf31b94a50de10d3b040
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-11-25 19:39:27 +0300

    Update py example.

--- scripts/examples/aurora_cli_lib.py
+++ scripts/examples/aurora_cli_lib.py
@@ -5,7 +5,7 @@ from aurora_cli.src.api.group_api import aurora_cli_api
 
 def main():
     result = aurora_cli_api('/app/info')
-    print(result)
+    print('Version connect Aurora CLI: {}'.format(result['value']['VERSION']))
 
 
 if __name__ == '__main__':
