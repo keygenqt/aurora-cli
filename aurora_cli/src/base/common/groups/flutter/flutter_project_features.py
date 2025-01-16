644c772fb20f4bd7b1b5f4f4cc226aed87a4638f
Vitaliy Zarubin <v.zarubin@omp.ru>
2025-01-16 13:15:37 +0300

    Fix format projects.

--- aurora_cli/src/base/common/groups/flutter/flutter_project_features.py
+++ aurora_cli/src/base/common/groups/flutter/flutter_project_features.py
@@ -60,11 +60,11 @@ def flutter_project_format_common(
     if files_h or files_cpp:
         files = []
         for file in files_h:
-            if '3rdpatry' in str(file):
+            if '3rdparty' in str(file):
                 continue
             files.append(file)
         for file in files_cpp:
-            if '3rdpatry' in str(file):
+            if '3rdparty' in str(file):
                 continue
             files.append(file)
         result = shell_cpp_format(files, flutter_tool_get_clang_format(is_bar))
@@ -99,7 +99,7 @@ def flutter_project_check_format_common(
     # if C++ files exist run clang-format format
     if files_h or files_cpp:
         for file in files_h:
-            if '3rdpatry' in str(file):
+            if '3rdparty' in str(file):
                 continue
             copy_file = path_temp_copy(file, temp_folder)
             shell_cpp_format([copy_file], flutter_tool_get_clang_format(is_bar))
@@ -108,7 +108,7 @@ def flutter_project_check_format_common(
                 echo_stdout(OutResultInfo(TextInfo.project_format_needs()))
                 return False
         for file in files_cpp:
-            if '3rdpatry' in str(file):
+            if '3rdparty' in str(file):
                 continue
             copy_file = path_temp_copy(file, temp_folder)
             shell_cpp_format([copy_file], flutter_tool_get_clang_format(is_bar))
